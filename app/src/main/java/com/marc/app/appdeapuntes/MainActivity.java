5a7263567682c2512c8ed96c31d1657cc1c96cb2
marcfs31 <marcfs31@protonmail.com>
2019-01-10 20:31:54 +0100

    Primeros pasos

--- app/src/main/java/com/marc/app/appdeapuntes/MainActivity.java
+++ app/src/main/java/com/marc/app/appdeapuntes/MainActivity.java
@@ -2,12 +2,17 @@ package com.marc.app.appdeapuntes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // El metodo onCreate se ejecuta cada vez que hay un cambio en el estado de ejcucion de la actividad
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Carga los componentes definidos en el xml activity_main
+
+        TextView texto = (TextView) findViewById(R.id.idTexto); // Instanciamos el componente de tipo TextView buscandolo por el id definido en el xml activity_main
+        // texto.setText("Hola!");
     }
 }
